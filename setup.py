b700f89b17a12f0837a10c973da722a7acc51e74
Alexander Tepe <alexander.tepe@hotmail.de>
2024-09-24 14:59:53 +0200

    added project structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="NEBULA"
+    version="0.1"
+    description="Error injection and quantized training lib for NNs"
+    author="Alexander Tepe"
+    author_email="alexander.tepe@hotmail.de"
+    packages=find_packages()
+    name=""
+)
