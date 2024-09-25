db6ce57897834531f4a1d5846d9f362129ddb8e1
Alexander Tepe <alexander.tepe@hotmail.de>
2024-09-25 19:39:45 +0200

    added functions and test stubs

--- NEBULA/facade.py
+++ NEBULA/facade.py
@@ -1,4 +1,5 @@
 from NEBULA.utils.logging import getLogger
+from NEBULA.core.legacy import flip_random_bits_in_model_weights, flip_single_number_float
 class Facade():
 
     def __init__(self) -> None:
@@ -6,3 +7,17 @@ class Facade():
 
     def hello(self) -> str:
         return "Hello World"
+
+    def legacy_flip_random_bits_in_model_weights(self, model, probability = 0.001, check=-1):
+        """
+        functions from provided script
+        just passing through the arguments
+        """
+        return flip_random_bits_in_model_weights(model, probability, check)
+
+    def legacy_flip_single_number_float(self, number_to_flip, data_type = 32, probability = 0.001, check = -1):
+        """
+        functions from provided script
+        just passing through the arguments
+        """
+        return flip_single_number_float(number_to_flip, data_type, probability, check)
