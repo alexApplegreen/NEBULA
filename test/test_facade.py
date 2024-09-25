f1f99037077dcc9fecd3f8f93410026a618fa5bd
Alexander Tepe <alexander.tepe@hotmail.de>
2024-09-25 19:49:19 +0200

    more testing and TODOs

--- test/test_facade.py
+++ test/test_facade.py
@@ -3,7 +3,18 @@ from NEBULA.facade import Facade
 
 class MainTest(unittest.TestCase):
 
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+        self._facade = Facade()
+
     def test_main(self):
-        facade = Facade()
-        retVal = facade.hello()
+        retVal = self._facade.hello()
         self.assertEqual(retVal, "Hello World")
+
+    def test_flipBits(self):
+        # TODO
+        self.assertTrue(True)
+
+    def test_flipWeightsInModel(self):
+        # TODO
+        self.assertTrue(True)
--- test/test_legacy.py
+++ test/test_legacy.py
@@ -4,7 +4,9 @@ from NEBULA.core.legacy import flip_random_bits_in_model_weights, flip_single_nu
 class LegacyTest(unittest.TestCase):
 
     def test_flipSingleNumberFloat(self):
+        # TODO
         self.assertTrue(True)
 
     def test_flipRandomBitsInModelWeights(self):
+        # TODO
         self.assertTrue(True)
