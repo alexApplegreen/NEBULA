ec9e571b6bdf40481a977d9c2105c14fa278a0c5
Alexander Tepe <alexander.tepe@hotmail.de>
2024-09-25 19:05:10 +0200

    remove getLogger from API facade

--- NEBULA/__init__.py
+++ NEBULA/__init__.py
@@ -1,12 +1,11 @@
 import pkg_resources
 from NEBULA.facade import Facade
-from NEBULA.utils.logging import getLogger, setLoggingLevel
+from NEBULA.utils.logging import setLoggingLevel
 
 logger = getLogger(__name__)
 logger.info(f"NEBULA VERSION: {pkg_resources.require('NEBULA')[0].version}")
 
 __all__ = [
     "Facade",
-    "getLogger",
     "setLoggingLevel"
 ]
