031f1cc957246996159fa59b2de2d0d5a599add1
Alexander Tepe <alexander.tepe@hotmail.de>
2024-10-04 15:33:20 +0200

    fixed module definition

---
+++ NEBULA/core/__init__.py
@@ -0,0 +1,6 @@
+from NEBULA.core.legacy import flip_random_bits_in_model_weights, flip_single_number_float
+
+__all__ = [
+    "flip_single_number_float",
+    "flip_random_bits_in_model_weights"
+]
