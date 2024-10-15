9248f8ee639c9521f3092fedbdac94b3c5893d76
Alexander Tepe <alexander.tepe@hotmail.de>
2024-10-15 14:50:28 +0200

    added Test

--- NEBULA/core/__init__.py
+++ NEBULA/core/__init__.py
@@ -1,8 +1,10 @@
 from NEBULA.core.legacy import flip_random_bits_in_model_weights, flip_single_number_float
 from NEBULA.core.injector import Injector
+from NEBULA.core.legacyInjector import LegacyInjector
 
 __all__ = [
     "flip_single_number_float",
     "flip_random_bits_in_model_weights",
-    "Injector"
+    "Injector",
+    "LegacyInjector"
 ]
--- test/test_injector.py
+++ test/test_injector.py
@@ -1,14 +1,19 @@
 import unittest
 
+from keras import Model
 import numpy as np
 
 from NEBULA.core.injector import Injector
+from NEBULA.utils.logging import getLogger
 from utils import ModelUtils
+
+import tensorflow.keras
 
 
 class InjectorTest(unittest.TestCase):
 
     _model = None
+    _logger = getLogger(__name__)
 
     def setUp(self):
         if self._model is None:
@@ -26,3 +31,29 @@ class InjectorTest(unittest.TestCase):
             if not allSame:
                 break
         self.assertFalse(allSame)
+
+    def test_reconstructModelWorks(self):
+        modelCopy = ModelUtils.ModelUtils.getBasicModel()
+
+        # overwrite copy weights with zeros
+        for idx, layer in enumerate(self._model.layers):
+            original_weights = layer.get_weights()
+            new_weights = [np.zeros(w.shape) for w in original_weights]
+            copyLayer = modelCopy.get_layer(index=idx)
+            copyLayer.set_weights(new_weights)
+
+        # bring layer objects into dict form
+        layers = list()
+        for idx, layer in enumerate(modelCopy.layers):
+            # Get the name of the layer
+            layer_name = layer.name
+            layer_weights = self._model.get_layer(index=idx).get_weights()
+            # Store the weights in the dictionary
+            layers.append((layer_name, layer_weights))
+
+        # self._logger.debug(layers)
+
+        Injector._reconstructModel(modelCopy, layers)
+
+        for orig, new in zip(self._model.get_weights(), modelCopy.get_weights()):
+            self.assertTrue(np.allclose(orig, new))
