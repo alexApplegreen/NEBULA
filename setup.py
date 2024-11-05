9369e5927543f7cf6876d31fc3be1461ad2f2080
Alexander Tepe <alexander.tepe@hotmail.de>
2024-11-05 15:05:12 +0100

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NEBULANN",
-    version="0.1",
+    version="0.2",
     description="Error injection and quantized training lib for NNs",
     author="Alexander Tepe",
     author_email="alexander.tepe@hotmail.de",
