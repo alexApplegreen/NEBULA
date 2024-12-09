6ad7b354d0539b7ac8c2afb8a4da8e93f87c34a4
Alexander Tepe <alexander.tepe@hotmail.de>
2024-12-09 19:30:12 +0100

    add sample for MNIST test

--- sample/mnist_sample.py
+++ sample/mnist_sample.py
@@ -1,7 +1,6 @@
 import numpy as np
 from PIL import Image
 from keras.datasets import mnist
-from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import to_categorical
 
 from NEBULA import Injector, loadFatModel
@@ -20,7 +19,28 @@ if __name__ == '__main__':
 
     # Load the pre-trained model from .h5 file either with or without FAT
     model = loadFatModel('sampledata/fat_mnist_model.h5', compile=False)
+    weightsBefore = model.get_weights()
 
+    # inject error to FAT model
+    injector = Injector(model.layers, probability=0.00005)
+    injector.injectError(model)
 
+    # Preprocess the image data
+    # Load and resize image to 28x28 pixels
+    img = Image.open('sampledata/six.png').resize((28, 28))
 
-    model.summary()
+    # Convert to grayscale
+    img = img.convert('L')  # 'L' mode means grayscale
+
+    # Convert to NumPy array and normalize pixel values (0-1 range)
+    img_array = np.array(img) / 255.0
+
+    # Reshape the array to match MNIST model input (28, 28, 1)
+    img_array = img_array.reshape(1, 28, 28, 1)
+
+    preds = model.predict(img_array)
+    print(preds)
+
+    max = np.max(preds)
+    output = np.where(preds == max)
+    print(f"Prediction: {output[1]}, certainty: {max}")
